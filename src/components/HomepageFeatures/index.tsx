4128571aefd1172248c167e7acd402f5db98e6a9
Gift Nnko <nnkogift@gmail.com>
2025-09-22 15:57:18 +0300

    chore: updated app icons

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -15,7 +15,6 @@ const FeatureList: FeatureItem[] = [
 				icon: "img/logos/scorecard.png",
 				description: (
 						<>
-								The scorecard app is a performance monitoring tool that allows users to track the comparative performance of indicators over time and against different organization units and/or levels. With easy to use colour coded outputs, the scorecard is meant to allow users to make data-driven decisions using the performance outlined on via the scorecard app’s outputs.
 						</>
 				),
 		},
@@ -24,7 +23,6 @@ const FeatureList: FeatureItem[] = [
 				icon: "img/logos/bna.png",
 				description: (
 						<>
-								Bottleneck analysis (BNA) is a structured analysis of the determinants of coverage for a wide range of interventions delivered through the health sector, useful to supporting targeted operational planning. It is a systematic, outcome-based approach to equitable health programming and real-time monitoring that strengthens the health system, complementing and building on what exists.
 						</>
 				),
 		},
@@ -33,7 +31,6 @@ const FeatureList: FeatureItem[] = [
 				icon: "img/logos/bna-action-tracker.png",
 				description: (
 						<>
-								The action tracker is a management tool that helps improve effective coverage of priority interventions at district and community level, covering various intervention such as maternal, newborn, and child health (MNCH), HIV, and nutrition.
 						</>
 				),
 		},
@@ -42,7 +39,6 @@ const FeatureList: FeatureItem[] = [
 				icon: "img/logos/standalone-action-tracker.png",
 				description: (
 						<>
-								The action tracker is a management tool that helps improve effective coverage of priority interventions at district and community level, covering various interventions such as maternal, newborn, and child health (MNCH), HIV, and nutrition.
 						</>
 				),
 		},
